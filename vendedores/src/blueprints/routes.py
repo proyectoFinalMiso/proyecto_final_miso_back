d576d02f37cdbd39ed68eba0ed86629000c46ea1
orange <da.naranjo10@uniandes.edu.co>
2025-03-18 20:41:53 -0500

    endpoint crear vendedor

--- vendedores/src/blueprints/routes.py
+++ vendedores/src/blueprints/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from src.commands.health_check import HealthCheck
+from src.commands.crear_vendedor import CrearVendedor
 
 blueprint = Blueprint('productos', __name__)
 
@@ -7,3 +8,8 @@ blueprint = Blueprint('productos', __name__)
 def health_check():
     return HealthCheck().execute()
 
+@blueprint.post('/crear_vendedor')
+def crear_vendedor():
+    body = request.get_json()
+    response = CrearVendedor(body).execute()
+    return jsonify(response['response']), response['status_code']
---
+++ vendedores/src/commands/crear_vendedor.py
@@ -0,0 +1,96 @@
+import re
+from uuid import uuid4
+
+from src.commands.base_command import BaseCommand
+from src.models.model import db, Vendedor
+
+class CrearVendedor(BaseCommand):
+
+    def __init__(self, request_body: dict):
+        self.vendedor_template = request_body
+
+    def crear_uuid(self) -> str:
+        return str(uuid4())
+    
+    def check_campos_requeridos(self) -> bool:
+
+        if self.vendedor_template.get('nombre') and self.vendedor_template.get('email'):
+            return True
+        else:
+            return False
+        
+    def verificar_vendedor_existe(self) -> bool:
+        
+        existe_vendedor_query = Vendedor.query.filter(
+            Vendedor.email == self.vendedor_template['email']
+        ).first()
+
+        if existe_vendedor_query:
+            return True
+        else:
+            return False
+        
+    def verificar_email_valido(self) -> bool:
+        
+        patron_email = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+        if re.match(patron_email, self.vendedor_template['email']):
+            return True
+        else:
+            return False
+        
+    def execute(self):
+
+        if not self.check_campos_requeridos():
+            return {
+                "response": {
+                    "msg": "Campos requeridos no cumplidos"
+                },
+                "status_code": 400
+            }
+        
+        if self.verificar_vendedor_existe():
+            return {
+                "response": {
+                    "msg": "Vendedor ya existe"
+                },
+                "status_code": 409
+            }
+        
+        if not self.verificar_email_valido():
+            return {
+                "response": {
+                    "msg": "Email invalido"
+                },
+                "status_code": 400
+            }
+        
+        id_vendedor = self.crear_uuid()
+
+        nuevo_vendedor = Vendedor(
+            id=id_vendedor,
+            nombre=self.vendedor_template['nombre'],
+            email=self.vendedor_template['email']
+        )
+
+        db.session.add(nuevo_vendedor)
+
+        try:
+            db.session.commit()
+            return {
+                "response": {
+                    "msg": "Vendedor creado con exito"
+                },
+                "status_code": 201
+            }
+        except Exception as e:
+            db.session.rollback()
+            return {
+                "response": {
+                    "msg": "Error al crear vendedor"
+                },
+                "status_code": 500
+            }
+        
+
+
