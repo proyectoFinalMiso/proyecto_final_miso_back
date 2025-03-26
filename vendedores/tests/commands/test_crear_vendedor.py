07c8e4f54f38ea20836dfa6c6656b37e4058e6f5
orange <da.naranjo10@uniandes.edu.co>
2025-03-25 20:21:51 -0500

    test modulo vendedores endpoint healthcheck y crear_vendedores

---
+++ vendedores/tests/commands/test_crear_vendedor.py
@@ -0,0 +1,33 @@
+import pytest
+import json
+from faker import Faker
+
+from app import app
+from src.commands.crear_vendedor import CrearVendedor
+from src.commands.base_command import BaseCommand
+
+class TestCrearVendedor():
+
+    @pytest.fixture(scope='module')
+    def gen_request(self):
+        fake = Faker()
+        request_bodies = []
+
+        for i in range(10):
+            request_body = {
+                'nombre': fake.name(),
+                'email': fake.email()
+            }
+            request_bodies.append(request_body)
+
+        return request_bodies
+    
+    def test_base_model_inherit(self, gen_request):
+        route = CrearVendedor(gen_request[0])
+        assert isinstance(route, BaseCommand)
+
+    def test_crear_vendedor(self, gen_request):
+        with app.test_client() as client:
+            response = client.post('/crear_vendedor', json=gen_request[0])
+            assert response.status_code == 201
+            assert response.json == {"msg": "Vendedor creado con exito"}
