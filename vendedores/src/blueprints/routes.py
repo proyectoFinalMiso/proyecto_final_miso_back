ee2c96e103d1c841a5c1be2e9162816ac18e3307
OrangeScript <da.naranjo10@uniandes.edu.co>
2025-03-26 19:52:14 -0500

    endpoint obtener vendedor

--- vendedores/src/blueprints/routes.py
+++ vendedores/src/blueprints/routes.py
@@ -1,15 +1,22 @@
 from flask import Blueprint, jsonify, request
 from src.commands.health_check import HealthCheck
 from src.commands.crear_vendedor import CrearVendedor
+from src.commands.obtener_vendedor import ObtenerVendedor
 
 blueprint = Blueprint('productos', __name__)
 
-@blueprint.get('/ping')
+@blueprint.get('/vendedor/ping')
 def health_check():
     return HealthCheck().execute()
 
-@blueprint.post('/crear_vendedor')
+@blueprint.post('/vendedor/crear_vendedor')
 def crear_vendedor():
     body = request.get_json()
     response = CrearVendedor(body).execute()
+    return jsonify(response['response']), response['status_code']
+
+@blueprint.get('/vendedor/obtener_vendedor')
+def obtener_vendedor():
+    body = request.get_json()
+    response = ObtenerVendedor(body).execute()
     return jsonify(response['response']), response['status_code']
