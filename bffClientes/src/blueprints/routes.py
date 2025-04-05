e39eeb8904cf9bfd3602e3302e8006e5c02686f3
Simón Buriticá <s.buritica@uniandes.edu.co>
2025-04-05 18:56:04 -0500

    FIX: Refactor gestorClientes route in BFF to update variable naming and improve clarity

--- bffClientes/src/blueprints/routes.py
+++ bffClientes/src/blueprints/routes.py
@@ -4,7 +4,7 @@ from flask import Blueprint, jsonify, request
 
 blueprint = Blueprint('bffClientes', __name__)
 
-MS_PRODUCTOS_URL = rf"{getenv('MS_GESTOR_CLIENTES_URL')}"
+MS_GESTOR_CLIENTES_URL = rf"{getenv('MS_GESTOR_CLIENTES_URL')}"
 MS_PEDIDOS_URL = rf"{getenv('MS_PEDIDOS_URL')}"
 
 def handle_requests(host, path):
@@ -29,8 +29,8 @@ def health_check():
     return jsonify({"msg": 'La API del cliente móvil para clientes se encuentra operativa'}), 200
 
 @blueprint.route("/api/gestorClientes/<path:path>", methods=["GET", "POST"])
-def proxy_ms_productos(path=None):
-    response = handle_requests(MS_PRODUCTOS_URL, path)
+def proxy_ms_gestor_clientes(path=None):
+    response = handle_requests(MS_GESTOR_CLIENTES_URL, path)
     return jsonify(response.json()), response.status_code
 
 @blueprint.route("/api/gestorPedidos/<path:path>", methods=["GET", "POST"])
