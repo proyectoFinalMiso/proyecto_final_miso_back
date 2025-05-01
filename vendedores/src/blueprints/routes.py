db913ca6abdd84c52c75f15366a28f51081025aa
orange <da.naranjo10@uniandes.edu.co>
2025-04-30 22:09:34 -0500

    primeros endpoints plan de ventas

--- vendedores/src/blueprints/routes.py
+++ vendedores/src/blueprints/routes.py
@@ -4,6 +4,8 @@ from src.commands.crear_vendedor import CrearVendedor
 from src.commands.obtener_vendedor import ObtenerVendedor
 from src.commands.login_vendedor import LoginVendedor
 from src.commands.listar_vendedores import ListarVendedores
+from src.commands.plan.crear_plan import CrearPlanVentas
+from src.commands.plan.listar_planes import ListarPlanes
 
 blueprint = Blueprint('productos', __name__)
 
@@ -31,4 +33,15 @@ def login():
 @blueprint.get('/listar_vendedores')
 def listar_vendedores():
     response = ListarVendedores().execute()
+    return jsonify(response['response']), response['status_code']
+
+@blueprint.post('/crear_plan_ventas')
+def crear_plan_ventas():
+    body = request.get_json()
+    response = CrearPlanVentas(body).execute()
+    return jsonify(response['response']), response['status_code']
+
+@blueprint.get('/listar_planes')
+def listar_planes():
+    response = ListarPlanes().execute()
     return jsonify(response['response']), response['status_code']
--- vendedores/src/models/model.py
+++ vendedores/src/models/model.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from enum import Enum
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Identity
 
@@ -11,3 +12,32 @@ class Vendedor(db.Model):
     nombre = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     contrasena = db.Column(db.String, nullable=False)
+
+class EstadoPlan(Enum):
+    INICIADO = "INICIADO"
+    FINALIZADO = "FINALIZADO"
+
+class PlanVentas(db.Model):
+    __tablename__ = 'planes_ventas'
+
+    id = db.Column(db.String, primary_key=True)
+    vendedor_id = db.Column(db.String, db.ForeignKey('vendedores.id'), nullable=False)
+    vendedor_nombre = db.Column(db.String, nullable=False)
+    estado = db.Column(db.Enum(EstadoPlan), nullable=False)
+    fechaInicio = db.Column(db.DateTime, default=datetime.now())
+    fechaFinal = db.Column(db.DateTime, nullable=True)
+    metaVentas = db.Column(db.Integer, nullable=False)
+    productosPlan = db.Column(db.String, nullable=False)
+    # productosVendidos = db.Column(db.String, nullable=False)
+
+# class ReporteVendedores(db.Model):
+#     __tablename__ = 'reporte_vendedores'
+
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     vendedor_id = db.Column(db.String, db.ForeignKey('vendedores.id'), nullable=False)
+#     estado = db.Column(db.Enum(EstadoPlan), nullable=False)
+#     fechaInicio = db.Column(db.DateTime, default=datetime.now())
+#     fechaFinal = db.Column(db.DateTime, nullable=False)
+#     metaVentas = db.Column(db.Integer, nullable=False)
+
+#     vendedor = db.relationship('Vendedor', backref='reportes')
